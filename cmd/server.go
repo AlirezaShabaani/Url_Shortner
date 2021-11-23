aff41b3362e7d90edfcb422114253a49b85bee41
alirezashabaani <alireza.shabaani@gmail.com>
2021-11-23 13:17:14 +0330

    85 %

--- cmd/server.go
+++ cmd/server.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"github.com/labstack/echo/v4"
 	"github.com/spf13/cobra"
 	"url_shortner/config"
 	"url_shortner/internal/adapters/driven/mysql"
@@ -17,6 +18,7 @@ var (
 		Short: "shorting urls",
 		Long:  "shorting long urls",
 		Run: func(cmd *cobra.Command, args []string) {
+			e := echo.New()
 			mysqlConf,redisConf,serverConf := config.LoadConfigs()
 			MYSQL := mysql.InitMysql(mysqlConf.Host,mysqlConf.Port,mysqlConf.UserName,mysqlConf.Password,mysqlConf.DbName)
 			REDIS := redis.InitRedis(redisConf.Addr,redisConf.UserName,redisConf.Password,redisConf.DbName)
@@ -24,7 +26,10 @@ var (
 			redrepo := urlservices.NewCache(REDIS)
 			urlservices := service.New(dbrepo,uidgen.UIDGen().New(),redrepo)
 			hdl := httphdl.New(urlservices)
+			e.POST("/new",hdl.Save)
+			e.POST("/redirect",hdl.Read)
 
+			e.Logger.Fatal(e.Start(serverConf.Host + ":" + serverConf.Port))
 		},
 	}
 )
--- internal/adapters/driving/httphdl/routes.go
+++ internal/adapters/driving/httphdl/routes.go
@@ -1,8 +1,1 @@
 package httphdl
-
-import "github.com/labstack/echo/v4"
-
-func registerRoutes(e *echo.Echo) {
-	e.POST("/new",)
-	e.POST()
-}
